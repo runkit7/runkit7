d513df0aab527b8aeef2413d7c96441cdef753ac
Ed Gifford <egifford@corexcel.com>
2019-03-12 10:18:43 -0400

    Small change for PHP 7.3 compiled with --enable-runkit-modify.
    I took guidance from these sources:
    https://externals.io/message/102338#102484
    https://github.com/m6w6/ext-http/commit/512f733beac73f37ba4acbcf730ebc6c6de849b6

--- runkit.c
+++ runkit.c
@@ -303,7 +303,7 @@ ZEND_FUNCTION(_php_runkit_removed_method)
 	php_error_docref(NULL, E_ERROR, "A method removed by runkit was somehow invoked");
 }
 
-static inline void _php_runkit_init_stub_function(const char *name, void (*handler)(INTERNAL_FUNCTION_PARAMETERS), zend_function **result)
+static inline void _php_runkit_init_stub_function(const char *name, ZEND_NAMED_FUNCTION(handler), zend_function **result)
 {
 	*result = pemalloc(sizeof(zend_function), 1);
 	(*result)->common.function_name = zend_string_init(name, strlen(name), 1);  // TODO: Can this be persistent?
